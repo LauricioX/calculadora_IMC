a406baefb375d301ce7d579f45314077ea8acd73
Lauricio De Souza <86666254+LauricioX@users.noreply.github.com>
2022-03-17 22:16:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,45 @@ btn.addEventListener("click", function (){
 let nome = document.querySelector("#nome").value
 let altura = document.querySelector("#altura").value
 let peso = document.querySelector("#peso").value
-let msg = document.querySelector("#msg").innerHTML
+let msg = document.querySelector("#msg");
 
 if(nome !== "" && altura !== "" && peso !== ""){
 let rest = (peso / (altura * altura)).toFixed(1);
-alert(rest)
+
+let classi = ""
+  
+if(rest < 18){
+  classi = "abaixo do peso!"
+  limpar()
+}else if(rest < 24){
+  classi = " no peso normal!"
+  limpar()
+}else if(rest < 29){
+  classi = " em sobrepeso!"
+  limpar()
+}else if(rest < 34){
+  classi = " em grau de obesidade I! "
+  limpar()
+}else if(rest < 39){
+  classi = " em grau de obesidade II! "
+  limpar()
+}else {
+  classi = "em grau de obesidade III!"
+  limpar()
+}
+
+msg.innerHTML = nome + " seu IMC é " + rest + " e você está " + classi;
+
 
 }else{
-  msg = "oi"
+  msg.innerHTML = "preencha todos os campos !"
 }
 
 })
+
+function limpar(){
+  document.querySelector("#nome").value = ''
+   document.querySelector("#altura").value = ''
+   document.querySelector("#peso").value = ''
+   document.querySelector("#nome").focus()
+}
